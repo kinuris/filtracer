c5ff9a25bc315d23a9108fbbf65c2628ba563480
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2024-10-20 11:08:08 +0800

    Added change group image to chat module

--- resources/views/components/rename-group-modal.blade.php
+++ resources/views/components/rename-group-modal.blade.php
@@ -1,12 +1,22 @@
 <div id="renameGroupModal" class="fixed z-50 inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center hidden">
-    <div class="bg-white p-6 pt-0 rounded-lg shadow-lg w-1/3">
-        <!-- <img class="w-12 mx-auto" src="{{ asset('assets/leave.svg') }}" alt="Leave Group"> -->
-        <h2 class="text-lg font-semibold text-center mt-6">Rename Group</h2>
-        <form action="/chat/rename/{{ urlencode(request('initiate')) }}" method="POST">
-            @csrf
-            <?php
+    <?php
 
-            use App\Models\ChatGroup;
+    use App\Models\ChatGroup;
+
+    // NOTE: garbage logic ahh
+    if (request('initiate') && !is_numeric(request('initiate'))) {
+        $group = ChatGroup::query()->where('internal_id', '=', request('initiate'))->first();
+    }
+    ?>
+    <div class="bg-white p-6 pt-0 rounded-lg shadow-lg w-1/3">
+        <h2 class="text-lg font-semibold text-center mt-6">Edit Group</h2>
+        <form action="/chat/rename/{{ urlencode(request('initiate')) }}" enctype="multipart/form-data" method="POST">
+            @csrf
+            @if (isset($group))
+            <img src="{{ $group->image() }}" id="groupImagePreview" class="w-24 h-24 object-cover rounded-full shadow mx-auto my-4" alt="Preview">
+            @endif
+            <input id="groupImagePreviewSelector" name="profile_pic" type="file" id="profile-pic">
+            <?php
 
             $initiate = request('initiate');
             if ($initiate && !is_numeric($initiate)) {
