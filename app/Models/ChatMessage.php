0515f570d8c8488fd0f85d031dfdc504aec44da1
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2024-10-20 11:06:45 +0800

    Added file handling support

--- app/Models/ChatMessage.php
+++ app/Models/ChatMessage.php
@@ -15,6 +15,35 @@ class ChatMessage extends Model
         'chat_group_id'
     ];
 
+    public function getFileName() {
+        if ($this->type === 'text') {
+           throw new \Exception('Not a file or image'); 
+        }
+
+        return base64_decode(explode('.', $this->content)[1]);
+    }
+
+    public function isFile() {
+        if ($this->type === 'text') {
+            return false;
+        }
+
+        $file = mime_content_type('storage/chat/files/' . $this->content);
+
+        return $file  == 'application/pdf';
+    }
+
+
+    public function isImage() {
+        if ($this->type === 'text') {
+            return false;
+        }
+
+        $file = mime_content_type('storage/chat/files/' . $this->content);
+
+        return $file == 'image/png' || $file == 'image/jpg' || $file == 'image/jpeg';
+    }
+
     public function sender() {
         return $this->belongsTo(User::class, 'sender_id');
     }
