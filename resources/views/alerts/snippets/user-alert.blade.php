5b7a1498467d225ca805ec47a6d72e0d70184b8a
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2024-10-20 11:07:35 +0800

    Added individual alert completion to template

--- resources/views/alerts/snippets/user-alert.blade.php
+++ resources/views/alerts/snippets/user-alert.blade.php
@@ -1,5 +1,5 @@
 @foreach (auth()->user()->alerts()->latest()->get() as $alert)
-<a href="{{ $alert->action }}" class="flex flex-col px-3 py-1 border-b">
+<a href="/alert/complete/{{ $alert->id }}" class="flex flex-col px-3 py-1 border-b">
     <div class="flex justify-between mb-1">
         @if ($alert->is_read)
         <p class="text-sm max-w-[calc(100%-3rem)] text-gray-400">{{ $alert->title }}</p>
