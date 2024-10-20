d15ef109fe62ab650c205cf43fa8f6124ed7c95a
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2024-10-20 11:01:43 +0800

    Added completeAlert for individual completion

--- app/Http/Controllers/AlertController.php
+++ app/Http/Controllers/AlertController.php
@@ -3,17 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
+use App\Models\UserAlert;
 use Illuminate\Support\Facades\Auth;
 
 class AlertController extends Controller
 {
-    public function genAlerts() {
+    public function genAlerts()
+    {
         return view('alerts.snippets.user-alert');
     }
 
-    public function seenAll() {
-        User::query()->find(Auth::user()->id)->alerts()->update(['is_read' => true]);
+    public function completeAlert(UserAlert $alert)
+    {
+        $alert->update(['is_read' => true]);
+
+        return redirect($alert->action);
+    }
+
+    public function seenAll()
+    {
+        User::query()->find(Auth::user()->id)
+            ->alerts()
+            ->update(['is_read' => true]);
 
         return back();
     }
